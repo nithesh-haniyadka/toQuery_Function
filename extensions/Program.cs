d398c7e2e157713e708a58b5685420d3523faa05
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-08-09 17:57:17 +0530

    Add project files.

---
+++ extensions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Extensions
+{
+
+    public static class StringExtensions
+    {
+        public static string Left(this string s, int count)
+        {
+            return s.Substring(0, count);
+        }
+
+        public static string Right(this string s, int count)
+        {
+            return s.Substring(s.Length - count, count);
+        }
+
+        public static string Mid(this string s, int index, int count)
+        {
+            return s.Substring(index, count);
+        }
+
+        public static int ToInteger(this string s)
+        {
+            int integerValue = 0;
+            int.TryParse(s, out integerValue);
+            return integerValue;
+        }
+
+        public static bool IsInteger(this string s)
+        {
+            Regex regularExpression = new Regex("^-[0-9]+$|^[0-9]+$");
+            return regularExpression.Match(s).Success;
+        }
+
+    }
+
+}
---
+++ toQuery_Function/Program.cs
@@ -0,0 +1,86 @@
+﻿//using Extensions;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+
+//namespace ExtendingStrings
+//{
+
+
+//    class Program
+//    {
+
+//        public static class ExtMetClass
+//        {
+//            public static int IntegerExtension(this string str)
+//            {
+//                return Int32.Parse(str);
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            string test;
+//            Console.WriteLine("Enter the string :");
+//            test = Console.ReadLine();
+//            int num = test.IntegerExtension();
+//            //l = findLength(test);
+
+//            //Console.Write("Length of the string is : {0}\n\n", l);
+
+
+
+
+
+//            //if (test.IsInteger())
+//            //{
+//            //    Console.WriteLine("value = {0}", test.IsInteger());
+//            //}
+
+
+//            Console.ReadLine();
+//        }
+
+//    }
+
+//}
+using System;
+using System.Text;
+namespace ExtensionMethod2
+{
+    public static class ExtMetClass
+    {
+        public static int ToCurrency(this string str)
+        {
+            return Int32.Parse(str);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter the string :");
+            str = Console.ReadLine();
+            try
+            {
+                Int32.TryParse(str, out int numValue);
+                    int num = str.ToCurrency();
+                    Console.WriteLine("$ {0}", num); 
+                
+            }
+            catch(FormatException e)
+            {
+                Console.Write(e.GetType());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
