e27a9dc575bda996494c7a65d3e8bfb9b7aaf1b3
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-08-09 17:59:12 +0530

    Assigment_4_toCurrency

--- toQuery_Function/Program.cs
+++ toQuery_Function/Program.cs
@@ -1,56 +1,8 @@
-﻿//using Extensions;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Text.RegularExpressions;
-
-
-//namespace ExtendingStrings
-//{
-
-
-//    class Program
-//    {
-
-//        public static class ExtMetClass
-//        {
-//            public static int IntegerExtension(this string str)
-//            {
-//                return Int32.Parse(str);
-//            }
-//        }
-//        static void Main(string[] args)
-//        {
-//            string test;
-//            Console.WriteLine("Enter the string :");
-//            test = Console.ReadLine();
-//            int num = test.IntegerExtension();
-//            //l = findLength(test);
-
-//            //Console.Write("Length of the string is : {0}\n\n", l);
-
-
-
-
-
-//            //if (test.IsInteger())
-//            //{
-//            //    Console.WriteLine("value = {0}", test.IsInteger());
-//            //}
-
-
-//            Console.ReadLine();
-//        }
-
-//    }
-
-//}
-using System;
+﻿using System;
 using System.Text;
-namespace ExtensionMethod2
+namespace useExtensionMethod
 {
-    public static class ExtMetClass
+    public static class ExtMethodClass
     {
         public static int ToCurrency(this string str)
         {
